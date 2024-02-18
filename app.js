a9e99015e60c043a27f94d18878ac089799615da
Chaitali Patel <chaitalipatel210720@gmail.com>
2024-02-17 23:06:39 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// app.js
+const express = require('express');
+
+const app = express();
+const SERVER_PORT = process.env.PORT || 3000;
+
+
+// Serve an HTML form
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+  });
+
+app.get('/product', (req, res) => {
+    res.sendFile(__dirname + '/views/product.html');
+  });
+  
+  // Start the server
+  app.listen(SERVER_PORT, () => {
+    console.log(`Server is running on port ${SERVER_PORT}`);
+  });
